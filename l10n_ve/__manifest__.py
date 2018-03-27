cd869f83b708270de97fab1e0ecbaf3c2b3b36c7
Iron Graterol <ironxp@gmail.com>
2018-03-27 00:41:50 -0400

    Actualizacion de estados, ver 1.2

--- l10n_ve/__manifest__.py
+++ l10n_ve/__manifest__.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+# Copyright (C) 2017 Engineer & Nano Group, C.A. (<http://www.eyngroup.com.ve>).
+
 {
     'name': 'Venezuela - Accounting',
-    'version': '1.0',
-    'author': 'Eyngroup',
-    'website': 'https://eyngroup/ven-nif',
+    'version': '1.2',
     'category': 'Localization',
     'description': """
+Venezuela Accounting Basic Module
+
 General Chart of Accounts VEN-NIF.
-Provide Templates for Chart of Accounts , Taxes for Venezuela.""",
-    'depends': [
-        'account',
-        'base_iban',
-        'base_vat',
-    ],
+Provide Templates for Chart of Accounts , Taxes, Regional Currencies, States""",
+    'author': 'Iron Graterol',
+    'website': 'http://www.eyngroup.com.ve',
+    'depends': ['account','base_iban','base_vat'],
     'data': [
         'data/l10n_ve_chart_data.xml',
         'data/account.account.template.csv',
         'data/account.chart.template.csv',
         'data/account.account.tag.csv',
         'data/account.tax.group.csv',
+        'data/res_country_state_data.xml',
         'data/account.tax.template.csv',
-        'data/res.country.state.csv',
+        'data/account.fiscal.position.template.csv',
+        'data/res.bank.csv',
         'data/account_chart_template_data.yml',
     ],
     'demo' : ['demo/l10n_ve_demo.xml'],
