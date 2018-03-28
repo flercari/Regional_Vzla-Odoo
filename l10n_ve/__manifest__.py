39820bae88845316aa809956d920acb5161cfa2e
Iron Graterol <ironxp@gmail.com>
2018-03-28 01:01:52 -0400

    reinicio para cambios de archivos

--- l10n_ve/__manifest__.py
+++ l10n_ve/__manifest__.py
@@ -3,36 +3,21 @@
 
 # Copyright (C) 2017 Engineer & Nano Group, C.A. (<http://www.eyngroup.com.ve>).
 
-{
-    'name': 'Venezuela - Accounting',
-    'version': '1.3',
+{	
+	'name': 'Venezuela - Accounting',
+    'version': '1.0',
     'category': 'Localization',
-    'description': """
-Venezuela Accounting Basic Module
-
-General Chart of Accounts VEN-NIF.
-Provide Templates for Chart of Accounts , Taxes, Regional Currencies, States""",
+    'description': """  TEST 1.4 """,
     'author': 'Iron Graterol',
     'website': 'http://www.eyngroup.com.ve',
-    'depends': ['account','base_iban','base_vat'],
+    'depends': ['account'],
     'data': [
-        'data/res_country_state_data.xml'
-    ],
-    'demo' : ['demo/l10n_ve_demo.xml']
-}
-
-'''
-    'data': [
-        'data/account_tax_group_data.csv',
-        'data/account_account_tag_data.csv',
-        'data/account_account_template_data.csv',
-        'data/account_chart_template_data.csv',
+        'data/l10n_ve_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account.chart.template.csv',
+        'data/account.account.tag.csv',
+        'data/account.tax.group.csv',		
+        'data/account.tax.template.csv',
         'data/account_chart_template_data.yml',
-        'data/account_fiscal_position_template.data.csv',
-        'data/account_tax_template_data.csv',
-        'data/l10n.ve_chart_data.xml',
-        'data/res_bank.csv',
-        'data/res_country_state_data.xml',
-        'data/res_currency_data.xml'
-    ],
-    '''
+	],
+}
