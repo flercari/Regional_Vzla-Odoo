5e96b10d12bac3bf25007da50e33d6edd8d261bf
Iron Graterol <ironxp@gmail.com>
2018-03-29 21:30:56 -0400

    lista de bancos actualizada

--- l10n_ve/__manifest__.py
+++ l10n_ve/__manifest__.py
@@ -4,19 +4,25 @@
 # Copyright (C) 2017 Engineer & Nano Group, C.A. (<http://www.eyngroup.com.ve>).
 
 {	
-	'name': 'Venezuela - Accounting',
+    'name': 'Venezuela - Accounting',
     'version': '1.0',
     'category': 'Localization',
-    'description': """  TEST BASE """,
+    'description': """
+Venezuela Accounting Basic Module
+
+General Chart of Accounts VEN-NIF.
+Provide Templates for Chart of Accounts, Taxes, Regional Currencies, States, List Bank""",
     'author': 'Iron Graterol',
     'website': 'http://www.eyngroup.com.ve',
     'depends': ['account'],
     'data': [
         'data/l10n_ve_chart_data.xml',
+		'data/res_country_state_data.xml',
+		'data/res.bank.csv',
         'data/account.account.template.csv',
         'data/account.chart.template.csv',
         'data/account.account.tag.csv',
-        'data/account.tax.group.csv',		
+        'data/account.tax.group.csv',
         'data/account.tax.template.csv',
         'data/account_chart_template_data.yml',
 	],
