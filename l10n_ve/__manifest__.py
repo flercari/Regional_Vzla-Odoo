118fa18b8a9b2470f060fb1b53f4dafdc68bee68
Iron Graterol <ironxp@gmail.com>
2018-04-03 02:33:27 -0400

    Actualizacion de Informacion para Bancos

--- l10n_ve/__manifest__.py
+++ l10n_ve/__manifest__.py
@@ -5,7 +5,7 @@
 
 {	
     'name': 'Venezuela - Regionalization',
-    'version': '2.0',
+    'version': '2.1',
     'category': 'Localization',
     'description': """
 Venezuela Module for Regional Parameters
